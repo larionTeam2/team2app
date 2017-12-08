3e146051cccce8f3e8367c3c16486c43f7d27977
Tai Nguyen-Trung <taint@pc-317.local.elarion.com>
2017-12-08 13:40:18 +0700

    Complete layout login, index, register, about. and create scaffold user username:string password:digest

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  
+  resources :users
+  root 'home#index', as: "home_index"
+
+  get 'Home', to: "home#index"
+  get '/about', to: 'home#about'
+  get '/Register', to: 'users#new'
+  get '/Login', to: 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
