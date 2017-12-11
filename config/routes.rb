276db62a556179afb7600ea59f1817ee908c8249
Tai Nguyen-Trung <taint@pc-317.local.elarion.com>
2017-12-11 16:40:27 +0700

    Finish layout index with hover picture user and Footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do  
   resources :users
 
-  resources :homes
+  #resources :homes
   resources :pictures
   
   root 'home#index', as: "home_index"
 
-  get 'Home', to: "home#index"
+  #get '/Home', to: "home#index"
   get '/about', to: 'home#about'
   get '/Register', to: 'users#new'
   #get '/Login', to: 'users#index'
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   delete 'logout' => "session#destroy"
 
   get 'home' => 'home#index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
